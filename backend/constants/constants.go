701350b87c59d28e1185a4ef48f39b855a865faa
PieDaoChuan3 <piedaochuan.3@gmail.com>
2022-03-06 18:11:43 -0700

    a Cloud and React based Social Network

---
+++ backend/constants/constants.go
@@ -0,0 +1,10 @@
+package constants
+
+const (
+	USER_INDEX  = "user"
+	POST_INDEX  = "post"
+	ES_URL      = "http://34.125.45.88:9200"
+	ES_USERNAME = "hjl20120511"
+	ES_PASSWORD = "19980511Hjl"
+	GCS_BUCKET  = "pdc3-around-bucket"
+)
