a244ee5cb4f687bf3d97d786190750d18601c4eb
PieDaoChuan3 <piedaochuan.3@gmail.com>
2022-08-06 11:56:09 -0600

    delete password and username

---
+++ backend/constants/constants.go
@@ -0,0 +1,10 @@
+package constants
+
+const (
+	USER_INDEX  = "user"
+	POST_INDEX  = "post"
+	ES_URL      = "URL"
+	ES_USERNAME = "USERNAME"
+	ES_PASSWORD = "PASSWORD"
+	GCS_BUCKET  = "pdc3-around-bucket"
+)
